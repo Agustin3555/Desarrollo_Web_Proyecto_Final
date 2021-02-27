2056dbb1d38a3705e3ad770a0a4c20aeeb562fca
florensg <floo.g1096@gmail.com>
2021-02-26 21:34:09 -0300

    url

--- Proyecto_Final/Proyecto_Final/urls.py
+++ Proyecto_Final/Proyecto_Final/urls.py
@@ -14,11 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from Proyecto_Final import views
+from django.urls import path, include
+
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("home/", views.saludo, name="saludo"),
+    
+    path("home/", views.saludo),
+
+    path("post", include('apps.post.urls')),
+    path('registrate', include('apps.registrate.urls')),
+    path('crearpost', include('apps.crearpost.urls'))
 ]
---
+++ Proyecto_Final/apps/crearpost/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrearpostConfig(AppConfig):
+    name = 'crearpost'
