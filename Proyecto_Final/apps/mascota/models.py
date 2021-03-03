3521d411be4fb4a7b05966153b9f60d4ae266de5
florensg <floo.g1096@gmail.com>
2021-03-03 10:49:24 -0300

    correccion errores base de datos

--- Proyecto_Final/apps/mascota/models.py
+++ Proyecto_Final/apps/mascota/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from Proyecto_Final.apps.publicacion.models import Publicacion
-from Proyecto_Final.apps.usuario.models import Usuario
+from apps.publicacion.models import Publicacion
+from apps.usuario.models import Usuario
 
 
 class Mascota(models.Model):
     id_mascota = models.AutoField(primary_key=True)
 
-    foto = models.ImageField()
     publicacion = models.OneToOneField(Publicacion, on_delete=models.CASCADE, related_name="+", null=True)
     especie = models.CharField(max_length=25, null=False)
-    sexo_item = ('macho', 'hembra')
+    sexo_item = (('macho','macho'),('hembra','hembra'))
     sexo = models.CharField(max_length=30, blank=True, null=False, choices=sexo_item)
     raza = models.CharField(max_length=25, null=False)
     edad = models.IntegerField(null=False)
