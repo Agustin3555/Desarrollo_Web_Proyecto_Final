73ef6c559aa8f98b4042c37c35814b2d52ca8762
florensg <floo.g1096@gmail.com>
2021-03-03 10:58:24 -0300

    correccion de errores en direccion de importaciones

--- Proyecto_Final/apps/mensaje/models.py
+++ Proyecto_Final/apps/mensaje/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from Proyecto_Final.apps.mascota.models import Mascota
-from Proyecto_Final.apps.usuario.models import Usuario
+from apps.mascota.models import Mascota
+from apps.usuario.models import Usuario
 
 
 class Correo(models.Model):
