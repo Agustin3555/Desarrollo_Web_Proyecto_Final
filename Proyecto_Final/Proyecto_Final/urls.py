094bbc750ce8dc59cc6e28c472d90783eb5bd016
Agustin3555 <Agustin3555@hotmail.com>
2021-03-03 17:05:47 -0300

    actualización del views del registro

--- Proyecto_Final/Proyecto_Final/urls.py
+++ Proyecto_Final/Proyecto_Final/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
 
     path('admin/', admin.site.urls),
     path('', include('apps.home.urls')),
-    path('', include('apps.publicaciones.urls')),
+    path('', include('apps.registrate.urls')),
 ]
--- Proyecto_Final/apps/registrate/views.py
+++ Proyecto_Final/apps/registrate/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.core.checks import messages
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.template import loader
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from Proyecto_Final.apps.usuario.models import Usuario
 
 
-def registrate(request):
-	ctx = {
-	}
-	return render(request, "registrate.html", ctx)
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = Usuario
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+def registro(request):
+    context = {'formulario': CustomUserCreationForm}
+
+    if request.method == 'POST':
+
+        formulario = CustomUserCreationForm(data=request.POST)
+
+        if formulario.is_valid():
+            formulario.save()
+
+            user = authenticate(username=formulario.cleaned_data['username'],
+                                password=formulario.cleaned_data['password'])
+            login(request, user)
+            messages.success(request, "Te has registrado correctamente")
+
+            return redirect(to="home/")
+
+        context['formulario'] = formulario
+
+    template = loader.get_template("registrate.html")
+
+    return HttpResponse(template.render(context, request))
