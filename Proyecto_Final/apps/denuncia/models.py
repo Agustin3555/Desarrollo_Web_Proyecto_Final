ff338dd3835d3bd666ccbf473a5da7494898354a
Agustin3555 <Agustin3555@hotmail.com>
2021-03-05 14:10:16 -0300

    Actualizacion en los modelos

--- Proyecto_Final/apps/denuncia/models.py
+++ Proyecto_Final/apps/denuncia/models.py
@@ -5,8 +5,8 @@ from apps.usuario.models import Usuario
 class Denuncia(models.Model):
     id_denuncia = models.AutoField(primary_key=True)
 
-    usuario_denunciante = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="+", null=True)
-    usuario_denunciado = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="+", null=True)
+    usuario_denunciante = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="+", null=True)
+    usuario_denunciado = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="+", null=True)
     fecha = models.DateField(null=False)
     tipo = models.CharField(max_length=1, null=False)
     comentario = models.CharField(max_length=500, null=False)
--- Proyecto_Final/apps/mascota/models.py
+++ Proyecto_Final/apps/mascota/models.py
@@ -6,12 +6,12 @@ from apps.usuario.models import Usuario
 class Mascota(models.Model):
     id_mascota = models.AutoField(primary_key=True)
 
-    publicacion = models.OneToOneField(Publicacion, on_delete=models.CASCADE, related_name="+", null=True)
+    publicacion = models.ForeignKey(Publicacion, on_delete=models.CASCADE, related_name="+", null=True)
     foto = models.ImageField(upload_to='media', null=False)
     especie = models.CharField(max_length=25, null=False)
     sexo = models.CharField(max_length=30, blank=True, null=False, choices=(('macho', 'Macho'), ('hembra', 'Hembra')))
     raza = models.CharField(max_length=25, null=False)
     edad = models.IntegerField(null=False)
     descripcion = models.CharField(max_length=100, null=False)
-    usuario_futuro_duenio = models.OneToOneField(Usuario, on_delete=models.CASCADE, related_name="+", null=True)
+    usuario_futuro_duenio = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name="+", null=True)
     estado = models.BooleanField(null=True)
